c97367b8edb99594b4c81de15efc89ab6f9d5ecc
Peter <rai.peter2@gmail.com>
2022-02-23 18:51:51 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from "spotify-web-api-js"
+import Player from './Player';
+import {useDataLayerValue} from "./DataLayer"
+
+const spotify = new SpotifyWebApi();
+  
+function App() {
+const [{user, token}, dispatch] = useDataLayerValue();
+
+// run code on a given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+   const _token = hash.access_token;
+
+if(_token){
+  spotify.setAccessToken(_token);
+
+  dispatch({
+    type:'SET_TOKEN',
+    token: _token
+  })
+
+  spotify.getMyTopArtists().then((response) =>
+        dispatch({
+          type: "SET_TOP_ARTISTS",
+          top_artists: response,
+        })
+      );
+
+      dispatch({
+        type: "SET_SPOTIFY",
+        spotify: spotify,
+      });
+
+  spotify.getMe().then(user => {
+    console.log("persom", user);
+    dispatch({
+      type:'SET_USER',
+      user : user
+    })
+  })
+
+  spotify.getUserPlaylists().then((playlists)=>{
+    dispatch({
+      type:"SET_PLAYLISTS",
+      playlists: playlists,
+
+    })
+  })
+  spotify.getPlaylist('3hwYcpgOUgICPsUW39dVSr').then(response =>{
+    dispatch({
+      type:'SET_DISCOVER_WEEKLY',
+      discover_weekly:  response
+    })
+  })
+}
+
+    console.log('I have a token>>>',token);
+  }, [])
+  return(
+    <div className="app">
+      {token ? <Player spotify={spotify} /> : <Login/>}
+    </div>
+  );
+}
+
+export default App;
